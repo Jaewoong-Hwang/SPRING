6340b5ba032e79714c205bdea6b86a334ef20863
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-04-24 08:59:31 +0900

    20250424

--- SPRINGMY/ex03_dataValidation/src/main/java/com/example/app/controller/MemoController.java
+++ SPRINGMY/ex03_dataValidation/src/main/java/com/example/app/controller/MemoController.java
@@ -2,6 +2,7 @@ package com.example.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -16,7 +17,7 @@ public class MemoController {
 		log.info("GET /memo/add...");
 	}
 	
-	@GetMapping("/add")
+	@PostMapping("/add")
 	public void add_post() {
 		log.info("POST /memo/add...");
 	}
