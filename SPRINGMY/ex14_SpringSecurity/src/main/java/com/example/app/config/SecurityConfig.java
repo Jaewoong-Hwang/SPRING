035f6cd1ebf02cca9745c9c9065c3e91addf87c4
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-04-29 16:49:03 +0900

    20250429

---
+++ SPRINGMY/ex14_SpringSecurity/src/main/java/com/example/app/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity  // security config 
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		//권한체크
+		http.authorizeRequests()
+			.anyRequest().authenticated();  // 어떤 위치로 가든 인증, 이대로 실행하면 403 -> 인증하지 않았기 때문에
+		
+		
+		
+		//로그인
+		http.formLogin()
+			.permitAll();
+		
+		//로그아웃
+		http.logout()
+			.permitAll();
+		
+		//예외처리
+		
+	
+	}
+
+	
+	@Override  //계정 임시로 만들기
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("user")
+			.password("{noop}1234") //암호화 알고리즘을 쓰지않은 옵션
+			.roles("USER"); //ROLE_ 이게 자동으로 붙음 --> ROLE_USER
+	}
+	
+	
+	
+}
