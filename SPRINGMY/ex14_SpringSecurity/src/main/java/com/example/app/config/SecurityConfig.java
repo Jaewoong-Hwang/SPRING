536bbe93716a2386c9594b15105c44a88dcd41b6
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-04-30 09:21:39 +0900

    20250430

--- SPRINGMY/ex14_SpringSecurity/src/main/java/com/example/app/config/SecurityConfig.java
+++ SPRINGMY/ex14_SpringSecurity/src/main/java/com/example/app/config/SecurityConfig.java
@@ -11,8 +11,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity  // security config 
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		//CSRF 비활성화
+		http.csrf().disable(); // + CSRF 토큰 값 확인 x, GET /logout 처리 가능
+		
 		//권한체크
 		http.authorizeRequests()
 			.anyRequest().authenticated();  // 어떤 위치로 가든 인증, 이대로 실행하면 403 -> 인증하지 않았기 때문에
@@ -36,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override  //계정 임시로 만들기
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-			.withUser("user")
+			.withUser("user1")
 			.password("{noop}1234") //암호화 알고리즘을 쓰지않은 옵션
 			.roles("USER"); //ROLE_ 이게 자동으로 붙음 --> ROLE_USER
 	}
